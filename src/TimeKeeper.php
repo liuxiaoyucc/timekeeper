b41c15d0c29eaab86e4461eb8aae83caf246f5da
xyliu <2199842341@qq.com>
2020-10-21 09:43:00 +0800

    第一版composer插件

---
+++ src/TimeKeeper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace liuxiaoyucc\timekeeper;
+
+/**
+ * 计时器
+ */
+class TimeKeeper
+{
+	public $startTime = 0;
+	public $endTime = 0;
+	public $consume = 0;
+
+	public function __construct()
+	{
+
+	}
+
+	private function init()
+	{
+		$this->consume = $this->endTime = $this->startTime = 0;
+	}
+
+	/**
+	 * 计时开始
+	 */
+	public function start()
+	{
+		$this->init();
+		return $this->startTime = microtime(1);
+	}
+
+	/**
+	 * 计时结束调用
+	 */
+	public function end()
+	{
+		return $this->endTime = microtime(1);
+	}
+
+	/**
+	 * 总用时
+	 */
+	public function consumeTime()
+	{
+		if (!$this->startTime) {
+			return 0;
+		}
+		$this->endTime = $this->endTime ? $this->endTime : microtime(1);
+		return $this->consume = bcsub($this->endTime, $this->startTime, 10) * 1000;
+	}
+}
